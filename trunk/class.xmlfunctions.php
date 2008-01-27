6ba877090fb7401a79f99acb1df24ad9b27257b4
kristopher <kristopher@685e968a-9948-0410-a9ba-b59daa9ddab0>
2008-01-27 02:56:35 +0000

    Renamed class ExtensioNotException to ExtensionNotInstalledException

--- trunk/class.xmlfunctions.php
+++ trunk/class.xmlfunctions.php
@@ -45,7 +45,7 @@
 		{
 			if( !class_exists( "DOMDocument" ) )
 			{
-				throw new ExtensioNotException( "Class DomDocument does not exist" );
+				throw new ExtensionNotInstalledException( "Class DomDocument does not exist" );
 			}
 		
 			$oDOM = new DOMDocument( "1.0" );
