ac87132cd14d587b3e6a0f204935451faa0338b9
kristopher <kristopher@685e968a-9948-0410-a9ba-b59daa9ddab0>
2008-02-22 01:40:14 +0000

    Added PayPalAPISale and PayPalAPIAuthorize classes, which extend and use the PayPalAPIPayment class.

---
+++ branches/paypal/class.paypalapiauthorize.php
@@ -0,0 +1,46 @@
+<?php
+/***************************************************************************************************
+ * OpenAvanti
+ *
+ * OpenAvanti is an open source, object oriented framework for PHP 5+
+ *
+ * @author			Kristopher Wilson
+ * @dependencies 	PayPalAPI
+ * @copyright		Copyright (c) 2008, Kristopher Wilson
+ * @license			http://www.openavanti.com/license
+ * @link			http://www.openavanti.com
+ * @version			0.6.4-alpha
+ *
+ */
+ 
+
+	/**
+	 * 
+	 *
+	 * @category	PayPal
+	 * @author		Kristopher Wilson
+	 * @link		http://www.openavanti.com/docs/paypal
+	 */
+	class PayPalAPIAuthorize extends PayPalAPIPayment
+	{		
+		private $oLastError = null;
+		private $sLastError = "";
+		
+		private $oProfile = null;
+		
+		
+		/**
+		 *
+		 */	
+		public function __construct( $sEnvironment, $sUserName, $sPassword, $sSignature )
+		{
+			parent::__construct( $sEnvironment, $sUserName, $sPassword, $sSignature );	
+		
+			$this->sType = "Authorize";
+		
+		} // __construct()
+		
+			
+	}; // PayPalAPIAuthorize()
+
+?>
