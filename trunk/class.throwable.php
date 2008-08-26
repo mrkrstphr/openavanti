7b5af7ca714451dcf588df422fb751b048603f84
kristopher <kristopher@685e968a-9948-0410-a9ba-b59daa9ddab0>
2008-08-26 02:36:18 +0000

    - Old, uncommitted updates

--- trunk/class.throwable.php
+++ trunk/class.throwable.php
@@ -1,4 +1,18 @@
 <?php
+/***************************************************************************************************
+ * OpenAvanti
+ *
+ * OpenAvanti is an open source, object oriented framework for PHP 5+
+ *
+ * @author			Kristopher Wilson
+ * @dependencies 	
+ * @copyright		Copyright (c) 2008, Kristopher Wilson
+ * @license			http://www.openavanti.com/license
+ * @link			http://www.openavanti.com
+ * @version			0.6.7-beta
+ *
+ */
+ 
 
 	/**
 	 *
