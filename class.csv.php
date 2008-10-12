2f8b4938bd70bb55e3c368383139dfe2b1c4b221
kristopherwilson <kristopherwilson@685e968a-9948-0410-a9ba-b59daa9ddab0>
2008-10-12 22:20:22 +0000

    - Added new method for adding an array of header data - Added method headers - Added code to handle data better and throw exceptions in some cases

--- class.csv.php
+++ class.csv.php
@@ -9,7 +9,6 @@
  * @license			http://www.openavanti.com/license
  * @link			http://www.openavanti.com
  * @version			0.6.7-beta
- *
  */
  
 	/**
@@ -26,8 +25,11 @@
 		
 		
 		/**
+		 * Adds a header to the list of CSV column headers. This method appends to the current list 
+		 * of headers by adding the single header.		 
 		 *
-		 *
+		 * @argument string The name of the header to add to the list of column headers		 
+		 * @returns void		 
 		 */		 		 		
 		public function AddHeader( $sHeader )
 		{
@@ -37,24 +39,64 @@
 		
 		
 		/**
-		 *
-		 *
+		 * Adds an array of headers to the list of CSV column headers. This method appends to the 
+		 * current list of headers by adding the passed array of headers to the existing array of
+		 * headers already added.
+		 *                   		 
+		 * @argument array An array of headers to append to the current array of headers
+		 * @returns void
+		 */
+        public function AddHeaders( $aHeaders )
+        {
+            if( is_array( $aHeaders ) && !empty( $aHeaders ) )
+            {
+                $this->aHeaders += $aHeaders;
+            }
+            
+        } // AddHeaders()                        	
+		
+		
+		
+		/**
+		 * Adds the supplied array of data to the CSV document. If the number of columns in the 
+		 * data does not match the number of columns in the headers (unless there are no headers),
+		 * this method will throw an exception.		 
+		 * 
+		 * @argument array An array of CSV row data
+		 * @returns void
 		 */		 		 		
 		public function AddData( $aData )
 		{
+            if( !empty( $this->aHeaders ) && count( $aData ) != count( $this->aHeaders ) )
+            {
+                throw new Exception( "Data column count does not match header column " . 
+                    "count in CSV data" );
+            }
+            
 			$this->aData[] = $aData;
 			
 		} // AddData()
 		
 		
 		/**
-		 *
-		 *
+		 * This method takes the headers and data stored in this object and creates a CSV
+		 * document from that data.		 
+		 * 		 
+		 * @returns the headers and data supplied as a string formatted as a CSV document
 		 */		 		 
 		public function __toString()
 		{
-			$sData = implode( ",", $this->aHeaders ) . "\n";
-			
+            $sData = "";
+            
+            // If headers are supplied, add them to the CSV string:
+            
+            if( !empty( $this->aHeaders ) )
+            {
+                $sData = implode( ",", $this->aHeaders ) . "\n";
+            }
+            
+            // Loop each row and convert it to a row of CSV data and add it to the CSV string:
+            
 			foreach( $this->aData as $aData )
 			{
 				$sDataRow = "";
