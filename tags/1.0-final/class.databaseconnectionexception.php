222f9535064d0e8650681406a2f403dff5398b27
kristopherwilson <kristopherwilson@685e968a-9948-0410-a9ba-b59daa9ddab0>
2009-06-26 21:10:26 +0000

    tagging 1.0 release

---
+++ tags/1.0-final/class.databaseconnectionexception.php
@@ -0,0 +1,28 @@
+<?php
+/***************************************************************************************************
+ * OpenAvanti
+ *
+ * OpenAvanti is an open source, object oriented framework for PHP 5+
+ *
+ * @author          Kristopher Wilson
+ * @dependencies    
+ * @copyright       Copyright (c) 2007-2009, Kristopher Wilson
+ * @license         http://www.openavanti.com/license
+ * @link            http://www.openavanti.com
+ * @version         1.0
+ *
+ */
+ 
+
+    /**
+     * This exception extends the PHP Exception class and should be thrown when an attempted 
+     * connection to a database fails.
+     * 
+     * See http://www.php.net/Exception for more information.
+     */         
+    class DatabaseConnectionException extends Exception
+    {
+    
+    } // DatabaseConnectionException()
+    
+?>
---
+++ tags/1.0-final/class.filenotfoundexception.php
@@ -0,0 +1,28 @@
+<?php
+/***************************************************************************************************
+ * OpenAvanti
+ *
+ * OpenAvanti is an open source, object oriented framework for PHP 5+
+ *
+ * @author          Kristopher Wilson
+ * @dependencies    
+ * @copyright       Copyright (c) 2007-2009, Kristopher Wilson
+ * @license         http://www.openavanti.com/license
+ * @link            http://www.openavanti.com
+ * @version         1.0
+ *
+ */
+ 
+
+    /**
+     * This exception extends the PHP Exception class and should be thrown when a needed file 
+     * does not exist at the specified location.
+     * 
+     * See http://www.php.net/Exception for more information.
+     */         
+    class FileNotFoundException extends Exception
+    {
+    
+    } // FileNotFoundException()
+    
+?>
