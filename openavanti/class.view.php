9ae0a61cc8db1636ee96c4005b7d063622ad7c41
kristopherwilson <kristopherwilson@685e968a-9948-0410-a9ba-b59daa9ddab0>
2009-12-27 18:13:38 +0000

    bumped version number and unified file headers

--- openavanti/class.view.php
+++ openavanti/class.view.php
@@ -1,12 +1,11 @@
 <?php
-/*******************************************************************************
+/***************************************************************************************************
  * OpenAvanti
  *
  * OpenAvanti is an open source, object oriented framework for PHP 5+
  *
  * @author          Kristopher Wilson
- * @dependencies    Controller
- * @copyright       Copyright (c) 2007-2009, Kristopher Wilson
+ * @copyright       Copyright (c) 2007-2010, Kristopher Wilson
  * @license         http://www.openavanti.com/license
  * @link            http://www.openavanti.com
  * @version         1.3.0-beta
