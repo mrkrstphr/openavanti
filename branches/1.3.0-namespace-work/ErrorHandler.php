4cd9f0e9f331f104d93df58481cdf66acdf46bec
kristopherwilson <kristopherwilson@685e968a-9948-0410-a9ba-b59daa9ddab0>
2010-01-24 22:06:55 +0000

    creating branch for 1.3.0-beta namespace conversion

---
+++ branches/1.3.0-namespace-work/ErrorHandler.php
@@ -0,0 +1,31 @@
+<?php
+/***************************************************************************************************
+ * OpenAvanti
+ *
+ * OpenAvanti is an open source, object oriented framework for PHP 5+
+ *
+ * @author          Kristopher Wilson
+ * @copyright       Copyright (c) 2007-2010, Kristopher Wilson
+ * @license         http://www.openavanti.com/license
+ * @link            http://www.openavanti.com
+ * @version         SVN: $Id$
+ */
+
+
+/**
+ * 
+ *
+ * @category    Database
+ * @author      Kristopher Wilson
+ * @link        http://www.openavanti.com/docs/errorhandler
+ */
+abstract class ErrorHandler
+{
+    const FILE_NOT_FOUND = "file_not_found";
+    const CONTROLLER_NOT_FOUND = "controller_not_found";
+    const LAYOUT_NOT_FOUND = "layout_not_found";
+    const VIEW_NOT_FOUND = "view_not_found";
+    
+}
+
+?>
