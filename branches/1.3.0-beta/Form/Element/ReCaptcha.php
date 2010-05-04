16d8db2de2f9231914d58dddf3feec0ca6d7b8df
kristopherwilson <kristopherwilson@685e968a-9948-0410-a9ba-b59daa9ddab0>
2010-05-04 17:24:53 +0000

    adding form element for easy generation for ReCaptchas

---
+++ branches/1.3.0-beta/Form/Element/ReCaptcha.php
@@ -0,0 +1,50 @@
+<?php
+/***************************************************************************************************
+ * OpenAvanti
+ *
+ * OpenAvanti is an open source, object oriented framework for PHP 5+
+ *
+ * @author          Kristopher Wilson
+ * @copyright       Copyright (c) 2007-2010, Kristopher Wilson
+ * @license         http://www.openavanti.com/license
+ * @link            http://www.openavanti.com
+ * @version         SVN: $Id$
+ */
+
+namespace OpenAvanti;
+
+/**
+ *
+ * @category    Forms
+ * @author      Kristopher Wilson
+ * @link        http://www.openavanti.com/docs/form
+ */
+class ReCaptcha extends FormElement
+{
+    /**
+     * Renders the form element as HTML and returns the HTML string
+     *
+     * @return string The HTML of the rendered form element
+     */
+    public function render()
+    {
+        $recaptcha = new \OpenAvanti\Service\ReCaptcha($this->_attributes['publicKey'], null);
+
+        return $recaptcha->getHtml();
+
+    } // render()
+
+    
+    /**
+     *
+     *
+     */
+    public function setPublicKey($key)
+    {
+        $this->_attributes['publicKey'] = $key;
+
+    } // setPublicKey()
+
+} // ReCaptcha()
+
+?>
